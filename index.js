f45774dcbfcc4a700b5d42bb7b77d14e09bf55d9
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2025-12-20 01:49:14 +0600

    added sercices api by filtering

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abef6se.mongodb.net/?appName=Cluster0`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abef6se.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,8 +29,7 @@ async function run() {
 
     const db = client.db("home_deco");
     const servicesCollection = db.collection("services");
-    
-  
+
     app.post("/services", async (req, res) => {
       const newServices = req.body;
       const result = await servicesCollection.insertOne(newServices);
@@ -42,30 +38,55 @@ async function run() {
 
     app.get("/services", async (req, res) => {
       try {
-        // const query = req.body;
-        const result = await servicesCollection.find().toArray();
-        // console.log("Matched products:", result.length);
+        const search = req.query.search;
+        const type = req.query.type; // category
+        const limit = parseInt(req.query.limit) || 0;
+        const min = parseInt(req.query.min);
+        const max = parseInt(req.query.max);
+        const query = {};
+
+        if (search) {
+          query.service_name = { $regex: search, $options: "i" }; 
+        }
+        if (type) {
+          query.service_category = { $regex: type, $options: "i" };
+        }
+        if (!isNaN(min) || !isNaN(max)) {
+          query["costs.0"] = {};
+
+          if (!isNaN(min)) {
+            query["costs.0"].$gte = min;
+          }
+          if (!isNaN(max)) {
+            query["costs.0"].$lte = max;
+          }
+        }
+
+        const result = await servicesCollection
+          .find(query)
+          .sort({ rating: -1 }) 
+          .limit(limit)
+          .toArray();
+
         res.send(result);
       } catch (error) {
-        // console.error("Error fetching products:", error);
-        res.status(500).send({ message: "Server error" });
+        console.error("Error fetching services:", error);
+        res.status(500).send({ error: "Server error" });
       }
     });
 
-     app.get("/services/:id", async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await servicesCollection.findOne(query);
       res.send(result);
     });
 
-      app.get("/top_rating", async (req, res) => {
+    app.get("/top_rating", async (req, res) => {
       const cursor = servicesCollection.find().sort({ rating: -1 }).limit(8);
       const result = await cursor.toArray();
       res.send(result);
     });
-
-   
 
     // await client.db("admin").command({ ping: 1 });
     console.log(
